63fac006e3c0097025ec10c85cdc82980dde61f1
Florian PASTEUR <florian.c.p@pm.me>
2023-10-25 23:24:40 +0200

    wip inline css

--- rise/insert-html.js
+++ rise/insert-html.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rise insert wistia transcript
 // @namespace    https://github.com/florianpasteur/tampermonkey-extensions
-// @version      0.3
+// @version      0.4
 // @description  Insert wistia transcript at caret
 // @author       Florian Pasteur
 // @match        https://rise.articulate.com/*
@@ -14,7 +14,7 @@
 
 // ==/UserScript==
 
-(async function() {
+(async function () {
     'use strict';
 
     function pasteHtmlAtCaret(html) {
@@ -31,7 +31,7 @@
                 var el = document.createElement("div");
                 el.innerHTML = html;
                 var frag = document.createDocumentFragment(), node, lastNode;
-                while ( (node = el.firstChild) ) {
+                while ((node = el.firstChild)) {
                     lastNode = frag.appendChild(node);
                 }
                 range.insertNode(frag);
@@ -51,57 +51,84 @@
         }
     }
 
-    function generateHtmlFromMarkdownWithCustomStyle(content) {
-        let htmlContent = `<!DOCTYPE html><head><style>
-p {
-    margin-bottom: 0.5em
+    async function generateHtmlFromMarkdownWithCustomStyle(content) {
+        let styleContent = `<style>
+.style p {
+    margin-bottom: 0.5em;
+    white-space: normal !important;
 }
-h1, h2, h3, h4, h5, h6 {
+.style h1, .style h2, .style h3, .style h4, .style h5, .style h6 {
     font-size: 1.5em;
     font-weight: 700;
 }
-</style></head><body>${marked.parse(content)}</body></html>`
-        return inlineAllCss(htmlContent, false)
+
+</style>`
+        let htmlContent = `<div class="style">${marked.parse(content)}</div>`
+        return await inlineAllCss(htmlContent, styleContent)
     }
 
-    function inlineAllCss(html, classStyleOnly) {
-        // const iframe = document.createElement('iframe');
-        // iframe.src = 'https://www.xylect.com/html/ifmPumpEmpty.htm';
-        // const _document = iframe.contentDocument
-        // const window = iframe.contentWindow
-        // document.body.append(iframe)
-        // _document.body.innerHTML = html;
-        const iframe = document.createElement('div');
-        const _document = document
-        document.body.append(iframe)
-        const main = iframe;
-        main.innerHTML = html
+    async function createEmptyIframe() {
+        return new Promise(resolve => {
+
+            const iframe = document.createElement('iframe');
+            iframe.src = 'about:blank';
+            iframe.height = '400px';
+            iframe.onload = () => {
+                resolve(iframe)
+            }
+            document.body.append(iframe)
+        })
+    }
+
+    async function inlineAllCss(html, styleContent) {
+        const iframe = await createEmptyIframe();
+        const _document = iframe.contentDocument
+        const window = iframe.contentWindow
+        const main = _document.body;
+        main.innerHTML = styleContent + html
+        const defaultStylePerElement = await getStandardStyle(html)
         main.querySelectorAll("*").forEach(e => {
             const computedStyle = window.getComputedStyle(e)
-            const element = _document.createElement(e.tagName)
-            main.append(element)
-            const defaultStyle = window.getComputedStyle(element)
+            const defaultStyle = defaultStylePerElement.get(e.tagName) || {};
             const inlineStyleDeclaration = Object.keys(computedStyle)
                 .filter(styleElement => !isNaN(parseInt(styleElement)))
                 .map(styleIndex => computedStyle[styleIndex])
-                .filter(
-                    styleElement =>
-                        !classStyleOnly ||
-                        computedStyle[styleElement] !== defaultStyle[styleElement]
-                )
-                .map(styleElement => styleElement + ":" + computedStyle[styleElement])
+                .filter(styleElement => defaultStyle[kebabCaseToCamelCase(styleElement)])
+                .filter(styleElement => {
+                    return computedStyle[kebabCaseToCamelCase(styleElement)] !== defaultStyle[kebabCaseToCamelCase(styleElement)];
+                })
+                .map(styleElement => `${styleElement}:${computedStyle[styleElement]}`)
                 .join(";")
+            debugger
             e.setAttribute("style", inlineStyleDeclaration)
-            element.remove()
         })
 
         const content = main.innerHTML
             .toString()
             .split("\n")
             .join("<br />");
-        main.remove();
+        // main.remove();
         return content
     }
+
+    async function getStandardStyle(html) {
+        const iframe = await createEmptyIframe();
+        const _document = iframe.contentDocument
+        const window = iframe.contentWindow
+        const main = _document.body;
+        main.innerHTML = `<style> body { width: auto; } </style>` + html
+        const defaultStylePerElement = new Map();
+        main.querySelectorAll("*").forEach(e => {
+            const element = _document.createElement(e.tagName)
+            main.append(element)
+            const defaultStyle = window.getComputedStyle(element);
+            defaultStylePerElement.set(e.tagName, {...defaultStyle})
+            element.remove()
+        })
+
+        return defaultStylePerElement;
+    }
+
 
     async function downloadTranscript(link) {
         const videoId = link.split('/').pop();
@@ -141,10 +168,18 @@ h1, h2, h3, h4, h5, h6 {
         }
         return '';
     }
-    let wistiaUrl = prompt("Please enter wistia URL");
+
+    let wistiaUrl = prompt("Please enter wistia URL", "https://backbase-11.wistia.com/medias/777ijlgisl");
 
     if (wistiaUrl != null) {
         const transcript = await downloadTranscript(wistiaUrl);
-        pasteHtmlAtCaret(generateHtmlFromMarkdownWithCustomStyle(transcript));
+        const html = await generateHtmlFromMarkdownWithCustomStyle(transcript);
+        pasteHtmlAtCaret(html);
+    }
+
+    function kebabCaseToCamelCase(str) {
+        return str.replace(/-([a-z])/g, function (match, group1) {
+            return group1.toUpperCase();
+        });
     }
 })();
