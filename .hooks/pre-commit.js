7a5a900d07659779c473fbd6a799662e9cad417d
Florian PASTEUR <florian.c.p@pm.me>
2023-11-08 15:25:01 +0100

    remove desc by accident

--- .hooks/pre-commit.js
+++ .hooks/pre-commit.js
@@ -22,6 +22,8 @@ async function updateDocumentation() {
 
 ![Icon](${metadata['@icon']})
 
+${metadata['@description']}
+
 [${metadata['filename']}](${metadata['relativePath']})
 
 ${insertScreenshotIfExists(metadata)}
