32c89c852611a4530b835436183e5021d6c1167f
Florian PASTEUR <florian.c.p@pm.me>
2023-11-08 13:09:15 +0100

    add content editable

---
+++ utils/edit-html-page.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Edit page
+// @namespace    https://github.com/florianpasteur/tampermonkey-extensions
+// @version      0.6
+// @description  Make entire page editable, useful to change data before taking screenshot
+// @author       Florian Pasteur
+// @match        *
+// @run-at       context-menu
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=js.org
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    document.body.setAttribute("contenteditable", "enable")
+})();
+
