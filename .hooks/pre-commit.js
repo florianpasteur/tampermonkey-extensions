1c81d01bc980ad3b98a013867ee0b73b4466a496
Florian PASTEUR <florian.c.p@pm.me>
2023-11-08 15:11:42 +0100

    generate doc automatically

--- .hooks/pre-commit.js
+++ .hooks/pre-commit.js
@@ -3,8 +3,90 @@ const fsSync = require('fs');
 const path = require('path');
 const os = require('os');
 
-(async function () {
 
+async function updateDocumentation() {
+
+    const allFiles = await getAllFilesRecursively('.');
+    const allMetadata = await getAllFilesMetadata(allFiles);
+
+    function insertScreenshotIfExists(metadata) {
+        const screenshotPath = 'docs/' + metadata.filename + '.png';
+        if (fsSync.existsSync(screenshotPath)) {
+            return `![Screenshot for ${metadata['@name']}](${screenshotPath})`
+        }
+        return ''
+    }
+
+    const documentation = allMetadata.map(metadata => `
+<h1>
+    <img src="${metadata['@icon']}" width="128" height="128" style="float:left;" alt="icon" />
+    ${metadata['@name']} // version ${metadata['@version']} 
+</h1>
+
+${metadata['@description']}
+
+${insertScreenshotIfExists(metadata)}
+----
+`).join('\n');
+
+    await replaceLivingDocumentationInFile(documentation, "README.md")
+
+
+    async function getAllFilesRecursively(dir, fileList = []) {
+        const files = await fs.readdir(dir);
+
+        for (const file of files) {
+            const filePath = path.join(dir, file);
+            const stat = await fs.stat(filePath);
+
+            if (stat.isDirectory()) {
+                await getAllFilesRecursively(filePath, fileList);
+            } else if (stat.isFile()) {
+                fileList.push(filePath);
+            }
+        }
+
+        return fileList;
+    }
+
+    async function getAllFilesMetadata(allFiles) {
+        const jsFiles = allFiles.filter(file => file.match(/.*\.js/));
+
+        const metadata = []
+        for (let file of jsFiles) {
+            const fileContent = (await fs.readFile(file)).toString();
+            if (!fileContent.startsWith("// ==UserScript==")) {
+                continue;
+            }
+            const tamperMonkeyMetadata = fileContent.split('\n').filter(line => line.startsWith("// @")).map(line => line.split(' ')).reduce((metadata, [_, metadataName, ...value]) => ({[metadataName]: value.filter(v => v).join(' '), ...metadata}), {});
+            metadata.push({...tamperMonkeyMetadata, filename: path.basename(file)})
+        }
+        return metadata;
+    }
+
+    async function replaceLivingDocumentationInFile(content, readMeFile) {
+        if (!fsSync.existsSync(readMeFile)) {
+            throw new Error(readMeFile + " does not exists");
+        }
+        const readMeFileContent = (await fs.readFile(readMeFile)).toString();
+
+        const existingLivingDocumentationSection = /<!-- start-living-doc -->((.|\n)*)<!-- end-living-doc -->/gm;
+        const newLivingDocumentationSection = `<!-- start-living-doc -->\n${content}\n<!-- end-living-doc -->`;
+
+        if (readMeFileContent.match(existingLivingDocumentationSection)) {
+            await fs.writeFile(readMeFile, readMeFileContent.replace(existingLivingDocumentationSection, newLivingDocumentationSection));
+        } else {
+            throw new Error("Couldn't find location for living documentation ie: <!-- start-living-doc --><!-- end-living-doc --> tags")
+        }
+    }
+}
+
+(async function () {
+    await bumpVersionOfCommitedFiles();
+    await updateDocumentation();
+})();
+
+async function bumpVersionOfCommitedFiles() {
     for (let file of process.argv) {
         if (fsSync.existsSync(file)) {
             let fileContent = (await fs.readFile(file)).toString();
@@ -34,5 +116,4 @@ const os = require('os');
             }
         }
     }
-
-})();
+}
--- README.md
+++ README.md
@@ -2,7 +2,105 @@
 
 Bunch of extensions for Tampermonkey
 
+<!-- start-living-doc -->
 
-# Decathlon country links
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=bamboohr.com" width="128" height="128" style="float:left;" alt="icon" />
+    Download Manager Report // version 0.31 
+</h1>
 
+Download manager hierarchy per user
+
+
+----
+
+
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=decathlon.nl" width="128" height="128" style="float:left;" alt="icon" />
+    Decathlon country links // version 0.5 
+</h1>
+
+Create a link to jump between dutch and french website of decathlon on the same product
+
+![Screenshot for Decathlon country links](docs/decathlon-country-links.js.png)
+----
+
+
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=docebo.com" width="128" height="128" style="float:left;" alt="icon" />
+    UserId on User search docebo // version 0.5 
+</h1>
+
+Add user id on the search table of users
+
+
+----
+
+
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=articulate.com" width="128" height="128" style="float:left;" alt="icon" />
+    Rise Content Downloader // version 0.6 
+</h1>
+
+Download rise course content as markdown files
+
+
+----
+
+
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=articulate.com" width="128" height="128" style="float:left;" alt="icon" />
+    Get rise-schema.json content // version 0.3 
+</h1>
+
+Copy list of lessons to be paste on the rise-schema.json for a markdown-to-rise-import
+
+
+----
+
+
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=articulate.com" width="128" height="128" style="float:left;" alt="icon" />
+    Rise insert wistia transcript // version 0.10 
+</h1>
+
+Insert wistia transcript at caret
+
+
+----
+
+
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=js.org" width="128" height="128" style="float:left;" alt="icon" />
+    Edit page // version 0.7 
+</h1>
+
+Make entire page editable, useful to change data before taking screenshot
+
+
+----
+
+
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=js.org" width="128" height="128" style="float:left;" alt="icon" />
+    Functions utils // version 0.6 
+</h1>
+
+A collection of utils function to be used in the terminal or in a JS script
+
+
+----
+
+
+<h1>
+    <img src="https://www.google.com/s2/favicons?sz=64&domain=zwift.com" width="128" height="128" style="float:left;" alt="icon" />
+    Zwift Fan Activity watcher // version 0.3 
+</h1>
+
+undefined
+
+
+----
+
+<!-- end-living-doc -->
 
